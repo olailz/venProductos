e59f75186cb265478c50536b9f8ffa7a764df807
olailz <olailz10.3@gmail.com>
2025-05-16 16:31:36 -0600

    actualizacion en inicio, inicio de sesion

--- js/script.js
+++ js/script.js
@@ -4,3 +4,11 @@ const navMenu = document.querySelector("nav ul");
 hamburguesa.addEventListener("click", () => {
     navMenu.classList.toggle("verMenu");
 });
+// Animación flip al hacer clic en imágenes
+document.querySelectorAll('.galeria img').forEach(img => {
+  img.addEventListener('click', () => {
+    img.classList.remove('flip'); // Reinicia si ya la tenía
+    void img.offsetWidth; // Fuerza reflow
+    img.classList.add('flip');
+  });
+});
